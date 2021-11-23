192a99e9ca8f500ebbd4bc17fab0f522985058b7
dolevav <dolevav@post.bgu.ac.il>
2021-11-23 23:09:07 +0200

    Upload ex5

---
+++ Animation1.js
@@ -0,0 +1,18 @@
+console.log("hhh");
+function thankyou() {
+    document.getElementById("Button").innerHTML = "Thank you";
+    console.log("thank you");
+}
+
+var x=5, y=7, T=(x==y);
+var z= "HI";
+console.log(T + z);
+
+function changePic() {
+    var pics = ["IMAGES/happy.jpg", "IMAGES/happy2.jpg"]; /*מערך -שיעור הבא תסביר*/
+    document.getElementById("HelAnimation").src= "IMAGES/happy2.jpg";
+}
+
+
+
+
---
+++ CVGrid.js
@@ -0,0 +1,5 @@
+console.log("Avia Dolev");
+function thankyou() {
+    document.getElementById("Button").innerHTML = "Thank you";
+    console.log("thank you, Avia dolev");
+}
