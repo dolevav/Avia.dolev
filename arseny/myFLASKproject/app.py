e6b161c6a55d5a6cf756324ea5f1eb678f01f69c
dolevav <dolevav@post.bgu.ac.il>
2021-12-25 21:08:09 +0200

    upload exc9

--- arseny/myFLASKproject/app.py
+++ arseny/myFLASKproject/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, url_for, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = '123'
 
 
 @app.route('/')
@@ -31,6 +32,35 @@ def ass8_func():
     #db
     return render_template('assignment8.html', my_profile={'name': 'avia', 'second': 'dolev'},
                            university='BGU', fav_books=['harry potter', 'me before you', 'jack richer'], hobbies=('music', 'sport', 'friends'))
+
+@app.route('/assignment9', methods=['GET', 'POST'])
+def ass9_func():
+    users = [
+    {'firstname': 'avia', 'secondname': 'dolev', 'age': '26'},
+    {'firstname': 'michal', 'secondname': 'lee', 'age': '22'},
+    {'firstname': 'sharon', 'secondname': 'cohen', 'age': '25'},
+    {'firstname': 'tal', 'secondname': 'fun', 'age': '33'},
+    {'firstname': 'mor', 'secondname': 'gil', 'age': '30'},
+    ]
+    if request.method == 'GET':
+        if 'firstname' in request.args:
+            firstname = request.args['firstname']
+            secondname = request.args['secondname']
+            age = request.args['age']
+            return render_template('assignment9.html', fn=firstname, sn=secondname, ag=age, users=users)
+        return render_template('assignment9.html')
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        #db
+        session['username'] = username
+        return render_template('assignment9.html', username=username)
+
+
+@app.route('/logout')
+def logout_func():
+    session['username'] = ''
+    return render_template('assignment9.html')
 
 
 
